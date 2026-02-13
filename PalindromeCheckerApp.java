0023f0e8826f44a95c3d319616b0645e5e9f0b06
GodwinSaji9 <sajigodwin44@gmail.com>
2026-02-13 10:59:20 +0530

    Display Welcome Message

---
+++ PalindromeCheckerApp.java
@@ -0,0 +1,6 @@
+public class PalindromeCheckerApp {
+    public static void main(String[] srgs){
+        System.out.println("Welcome to Palindrome Checker");
+        System.out.println("Version : 1.0");
+    }
+}
