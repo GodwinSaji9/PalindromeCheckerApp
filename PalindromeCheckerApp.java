011a802f9f505aac17a1923fb84623d84093a033
GodwinSaji9 <sajigodwin44@gmail.com>
2026-02-20 09:24:10 +0530

    Hardcoded string

--- PalindromeCheckerApp.java
+++ PalindromeCheckerApp.java
@@ -1,14 +1,31 @@
+import java.util.Scanner;
+
 /**
 Description:
- This class is Palindrome Checker Management System
+ This class demonstrates basic palindrome validation using a hardcoded string value
 
  @author Godwin
- @version 1.0
+ @version 2.0
 **/
 public class PalindromeCheckerApp {
-    public static void main(String[] srgs){
-        System.out.println("Welcome to Palindrome Checker");
-        System.out.println("Version : 1.0");
-        System.out.println("System Initialized successfully");
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        
+        System.out.print("Enter a string: ");
+        String input = sc.nextLine();
+        
+        String reversed = "";
+        
+        for (int i = input.length() - 1; i >= 0; i--) {
+            reversed += input.charAt(i);
+        }
+        
+        if (input.equalsIgnoreCase(reversed)) {
+            System.out.println("It is a palindrome.");
+        } else {
+            System.out.println("It is not a palindrome.");
+        }
+        
+        sc.close();
     }
 }
