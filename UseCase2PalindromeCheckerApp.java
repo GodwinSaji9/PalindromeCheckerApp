629ca9e11c6a2ca2a681342baa178cf01b6dc389
GodwinSaji9 <sajigodwin44@gmail.com>
2026-02-20 09:06:03 +0530

    Use Case 2

---
+++ UseCase2PalindromeCheckerApp.java
@@ -0,0 +1,31 @@
+/**
+Description:
+ This class demonstrates basic palindrome validation using a hardcoded string value
+
+ @author Godwin
+ @version 2.0
+**/
+
+import java.util.*;
+public class UseCase2PalindromeCheckerApp {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        
+        System.out.print("Enter a string: ");
+        String input = sc.nextLine();
+        
+        String reversed = "";
+        
+        for (int i = input.length() - 1; i >= 0; i--) {
+            reversed += input.charAt(i);
+        }
+        
+        if (input.equalsIgnoreCase(reversed)) {
+            System.out.println("It is a palindrome.");
+        } else {
+            System.out.println("It is not a palindrome.");
+        }
+        
+        sc.close();
+    }
+}
