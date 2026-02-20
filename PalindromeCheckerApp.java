5deb730c8ea5515d604c541bc756a313d7f0f89c
GodwinSaji9 <sajigodwin44@gmail.com>
2026-02-20 09:17:29 +0530

    Welcome Message

--- PalindromeCheckerApp.java
+++ PalindromeCheckerApp.java
@@ -1,6 +1,14 @@
+/**
+Description:
+ This class is Palindrome Checker Management System
+
+ @author Godwin
+ @version 1.0
+**/
 public class PalindromeCheckerApp {
     public static void main(String[] srgs){
         System.out.println("Welcome to Palindrome Checker");
         System.out.println("Version : 1.0");
+        System.out.println("System Initialized successfully");
     }
 }
