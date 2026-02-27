400e4cfa2187dd0886c11c934ba057d0879a1d4f
GodwinSaji9 <sajigodwin44@gmail.com>
2026-02-27 08:50:32 +0530

    Singly LL

--- PalindromeCheckerApp.java
+++ PalindromeCheckerApp.java
@@ -1,29 +1,20 @@
-/**
-Description:
-This class demonstrates palindrome validation using a queue and a stack
-
- @author Godwin
- @version 7.0
-**/
-
-import java.util.ArrayDeque;
-import java.util.Deque;
+import java.util.LinkedList;
 
 public class PalindromeCheckerApp {
     public static void main(String[] args) {
 
-        String input = "refer";
+        String input = "level";
 
-        Deque<Character> deque = new ArrayDeque<>();
+        LinkedList<Character> list = new LinkedList<>();
 
         for (char c : input.toCharArray()) {
-            deque.addLast(c);
+            list.add(c);
         }
 
         boolean isPalindrome = true;
 
-        while (deque.size() > 1) {
-            if (!deque.removeFirst().equals(deque.removeLast())) {
+        while (list.size() > 1) {
+            if (!list.removeFirst().equals(list.removeLast())) {
                 isPalindrome = false;
                 break;
             }
