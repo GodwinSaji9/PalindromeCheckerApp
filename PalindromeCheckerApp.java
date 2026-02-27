07828c300be8cc1d927c9ae8f8cab977264eb031
GodwinSaji9 <sajigodwin44@gmail.com>
2026-02-27 09:31:15 +0530

    Encapsulation

--- PalindromeCheckerApp.java
+++ PalindromeCheckerApp.java
@@ -1,35 +1,47 @@
 /**
 Description:
-This class demonstrates palindrome validation after preprocessing the input string 
+This class demonstrates palindrome validation by implementing encapsulation
 
  @author Godwin
- @version 10.0
+ @version 11.0
 **/
-public class PalindromeCheckerApp {
+class PalindromeCheckerApp {
+
+    public boolean checkPalindrome(String input) {
+
+        if (input == null)
+            return false;
+
+        String normalized = input.replaceAll("[^a-zA-Z0-9]", "").toLowerCase();
+
+        char[] arr = normalized.toCharArray();
+
+        int start = 0;
+        int end = arr.length - 1;
+
+        while (start < end) {
+
+            if (arr[start] != arr[end]) {
+                return false;
+            }
+
+            start++;
+            end--;
+        }
+
+        return true;
+    }
 
     public static void main(String[] args) {
 
-        String input = "A man, a plan, a canal: Panama";
-        
-        if (isPalindrome(input)) {
+        PalindromeCheckerApp checker = new PalindromeCheckerApp();
+
+        String input = "Level";
+
+        if (checker.checkPalindrome(input)) {
             System.out.println("It is a palindrome.");
         } else {
             System.out.println("It is not a palindrome.");
         }
-    }
-
-    public static boolean isPalindrome(String input) {
-
-        String normalized = input.replaceAll("[^a-zA-Z0-9]", "").toLowerCase();
-
-        for (int i = 0; i < normalized.length() / 2; i++) {
-
-            if (normalized.charAt(i) != 
-                normalized.charAt(normalized.length() - 1 - i)) {
-                return false;
-            }
-        }
-
-        return true;
     }
 }
